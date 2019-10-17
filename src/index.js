b6ae66d34507ffcc5871565e935f0553900c1226
Jake Lewochko <jlewochko@gmail.com>
2019-10-16 21:00:36 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
